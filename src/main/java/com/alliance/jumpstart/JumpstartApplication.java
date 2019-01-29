43fe5833ba78ebe50dd3d3d1e9ac1413a5614d26
pofay <gianCarlo.gilos@gmail.com>
2019-01-29 21:37:50 +0800

    Initial Commit

---
+++ src/main/java/com/alliance/jumpstart/JumpstartApplication.java
@@ -0,0 +1,31 @@
+package com.alliance.jumpstart;
+
+import com.alliance.jumpstart.entities.Customer;
+import com.alliance.jumpstart.repository.CustomerRepository;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class JumpstartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JumpstartApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner demo(CustomerRepository repository) {
+		return (args) -> {
+			// save a couple of customers
+			repository.save(new Customer("Jack", "Bauer"));
+			repository.save(new Customer("Chloe", "O'Brian"));
+			repository.save(new Customer("Kim", "Bauer"));
+			repository.save(new Customer("David", "Palmer"));
+			repository.save(new Customer("Michelle", "Dessler"));
+			
+		};
+	}
+
+}
---
+++ src/main/java/com/alliance/jumpstart/entities/Customer.java
@@ -0,0 +1,41 @@
+package com.alliance.jumpstart.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * Customer
+ */
+@Entity
+public class Customer implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    protected Customer() {}
+
+    public Customer(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+    
+    public Long getId() {
+        return id;
+    }
+}
